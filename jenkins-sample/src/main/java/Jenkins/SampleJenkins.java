57960b9618f40cc0d3730d646a3f0fd12eaeafac
Nassiha Nazeer <Nassiha Nazeer@LAPTOP-HDLLT077>
2023-12-14 13:18:09 +0530

    please commit

---
+++ jenkins-sample/src/main/java/Jenkins/SampleJenkins.java
@@ -0,0 +1,11 @@
+package Jenkins;
+
+public class SampleJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to jenkins");
+
+	}
+
+}
